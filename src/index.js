c5a0aad1a0037bc8316b870b407c4bbe1a07bc50
Anuj Goyal <goyal.anuj973@gmail.com>
2024-05-03 23:22:12 +0530

    issue solver for deploy

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// package.json-> "homepage": "https://goyalanuj973.github.io/anujportfolio", -> on top -> githubpages
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
